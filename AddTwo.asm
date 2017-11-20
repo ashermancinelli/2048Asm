8e7087ce13b7db2341d11aba9d527da52da097c1
Asher Mancinelli <30530525+ashermancinelli@users.noreply.github.com>
2017-11-19 18:22:09 -0800

    Update AddTwo.asm

--- AddTwo.asm
+++ AddTwo.asm
@@ -6,41 +6,120 @@ INCLUDE Irvine32.inc
 .data
 
 	; These coordinates are measured from 
-	playerPosX	BYTE 18
-	playerPosY	BYTE 11
-	deltax		SBYTE 0
-	deltay		SBYTE 0
+	playerPosX	BYTE 	18
+	playerPosY	BYTE 	11
+	currentRoom BYTE	520 dup(?)
+	deltax		SBYTE	0
+	deltay		SBYTE	0
 
-	startRoom	BYTE	"|-------------------------------|",0ah
-				BYTE	"|                               |",0ah
-				BYTE	"\                               |",0ah
-				BYTE	" \                              |",0ah
-				BYTE	"  \                             |",0ah
-				BYTE	"  /                             |",0ah
-				BYTE	" /                              |",0ah
-				BYTE	"/                               |",0ah
-				BYTE	"|                               |",0ah
-				BYTE	"|                               |",0ah
-				BYTE	"|                               |",0ah
-				BYTE	"|                 O             |",0ah
-				BYTE	"|-------------------------------|",0h
+	startRoom	BYTE	"|----------------- --------------------|",0ah
+				BYTE	"|                                      |",0ah
+				BYTE	"\                                      |",0ah
+				BYTE	" \                                     |",0ah
+				BYTE	"  \                                    |",0ah
+				BYTE	"  /                                    |",0ah
+				BYTE	" /                                     |",0ah
+				BYTE	"/                                      |",0ah
+				BYTE	"|                                      |",0ah
+				BYTE	"|                                      |",0ah
+				BYTE	"|                                      |",0ah
+				BYTE	"|                 O                    |",0ah
+				BYTE	"|--------------------------------------|",0h
 
 
-	room2		BYTE	"|-------------------------------|",0ah
-				BYTE	"\                               |",0ah
-				BYTE	" \                              |",0ah
-				BYTE	"  \                             |",0ah
-				BYTE	"   \                            |",0ah
-				BYTE	"    \                           |",0ah
-				BYTE	" ____\                          |",0ah
-				BYTE	"|                               |",0ah
-				BYTE	"|                               |",0ah
-				BYTE	"|                               |",0ah
-				BYTE	"|                               |",0ah
-				BYTE	"|                               |",0ah
-				BYTE	"|-------------------------------|",0h
+	room2		BYTE	"|--------------------------------------|",0ah
+				BYTE	"\                                      |",0ah
+				BYTE	" \                                     |",0ah
+				BYTE	"  \                                    |",0ah
+				BYTE	"   \                                   |",0ah
+				BYTE	"    \                                  |",0ah
+				BYTE	" ____\                                 |",0ah
+				BYTE	"|                                      |",0ah
+				BYTE	"|                                      |",0ah
+				BYTE	"|                                      |",0ah
+				BYTE	"|       O                              |",0ah
+				BYTE	"|                                      |",0ah
+				BYTE	"|--------------------------------------|",0h
 
 .code
+
+changeToR2 PROC
+
+	call Clrscr
+	mov edx, offset room2
+
+	mov esi, offset room2
+	mov edi, offset currentRoom
+	mov ecx, 520
+	rep movsb
+
+	call WriteString
+	
+	mov playerPosX, 8
+	mov playerPosY, 10
+	mov deltax, 0		
+	mov deltay, 0		
+
+	ret
+changeToR2 ENDP
+
+movePlayer PROC
+
+	;-------------------;
+	mov al, deltay		; al = (deltay+posY) * 40
+	add al, playerPosY	;
+	add al, playerPosY	;
+	add al, playerPosY	;
+	add al, playerPosY  ;
+	add al, playerPosY	;
+	add al, deltay		;
+	add al, deltay		;
+	add al, deltay		;
+	add al, deltay		;
+						;
+	movzx eax, al		;
+	and eax, eax		;
+	and eax, eax		;
+	and eax, eax		;
+	and eax, eax		;
+	;-------------------;
+
+	mov bh, deltax
+	add bh, playerPosX	
+
+	movzx ebx, bh
+	add ebx, eax
+
+
+	.if currentRoom[ebx] == 32
+
+		mov dh, playerPosY
+		mov dl, playerPosX
+		call Gotoxy
+		mov al, " "
+		call WriteChar
+
+		mov bh, deltax
+		mov bl, deltay
+
+		add playerPosX, bh
+		sub playerPosY, bl
+		mov dh, playerPosY
+		mov dl, playerPosX
+		call Gotoxy
+		mov al, "O"
+		call WriteChar
+
+		mov deltax, 0
+		mov deltay, 0
+
+		mov dh, 0
+		mov dl, 0
+		call Gotoxy
+	.endif
+	
+	ret
+movePlayer ENDP
 
 update PROC
 	
@@ -53,27 +132,12 @@ update PROC
 		mov deltay, -1
 	.elseif AL == 119
 		mov deltay, 1
+	.elseif AL == 99
+		call changeToR2
 	.endif
 
-	mov dh, playerPosY
-	mov dl, playerPosX
-	call Gotoxy
-	mov al, " "
-	call WriteChar
+	call movePlayer
 
-	mov bh, deltax
-	mov bl, deltay
-
-	add playerPosX, bh
-	sub playerPosY, bl
-	mov dh, playerPosY
-	mov dl, playerPosX
-	call Gotoxy
-	mov al, "O"
-	call WriteChar
-
-	mov deltax, 0
-	mov deltay, 0
 	ret
 update ENDP
 
@@ -81,8 +145,14 @@ update ENDP
 main PROC
 
 	mov edx, offset startRoom
+
+	mov esi, offset startRoom
+	mov edi, offset currentRoom
+	mov ecx, 520
+	rep movsb
+
 	call WriteString
-	mov ecx, 255
+	mov ecx, 3255
 	
 mainLoop:
 	call update
